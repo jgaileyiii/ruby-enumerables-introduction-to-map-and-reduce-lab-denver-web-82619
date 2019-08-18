bbe3fcb2e24066eadf05e22d5d57b417655f5d0b
Joseph Gailey <jgaileyiii@gmail.com>
2019-08-18 18:57:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,9 +35,7 @@ def map_to_square(source_array)
   return new
 end 
 def reduce_to_total(source_array,starting_point)
-  i = 0 
-  while source_array.length 
-  return source_array.sum
+  source_array
 end 
 
 
