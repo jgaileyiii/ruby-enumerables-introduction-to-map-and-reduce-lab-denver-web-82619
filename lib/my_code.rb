78ddbe39c937dd70f2efd96bd10f72de5c1b6fd4
Joseph Gailey <jgaileyiii@gmail.com>
2019-08-18 19:07:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,3 +37,6 @@ end
 def reduce_to_total(source_array, starting_point = 0)
     source_array.reduce(starting_point) {|sa ,  sp| sa + sp}
 end  
+def reduce_to_all_true(source_array)
+  source_array.all? 
+end  
